d4d945c4979bb709449fb9f1ca92a391442c1122
DanielDonato <daniel_donato10@yahoo.com.br>
2018-11-27 17:04:02 -0200

    Creating class Service and Injecting in HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService] // Componentes que serão de injetados NESTA CLASSE 
 })
 export class HomeComponent implements OnInit {
+  
+  public ofertas: Oferta[];
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { } //INJETANDO DEPENDENCIA
 
   ngOnInit() {
+    this.ofertas = this.ofertasService.getOfertas();
+    console.log(this.ofertas);
   }
 
 }
