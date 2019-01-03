675a9a2d8bc69436c22fc45f95e9d61b620826fc
DanielDonato <daniel_donato10@yahoo.com.br>
2019-01-03 18:49:40 -0200

    Http GET in ofertas with type restaurante

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -15,6 +15,12 @@ export class OfertasService {
                 .then((resposta:any) => resposta.json());
     }
 
+    public getOfertasPorCategoria(categoria:String): Promise<Oferta[]>{
+        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+            .toPromise()
+            .then((resposta:any) => resposta.json());
+    }
+
     /*public getOfertas2(): Promise<Oferta[]>{
         return new Promise((resolve, reject) => {
             let deuCerto = true;
