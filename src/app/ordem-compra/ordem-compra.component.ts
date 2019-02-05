f52c371bed842eaeece38657a3c64fd43eaf7016
DanielDonato <daniel_donato10@yahoo.com.br>
2019-02-05 18:06:40 -0200

    Validating dates in form

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -12,6 +12,21 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = '';
   public formaPagamento = '';
 
+  //controles de validaçao dos campos
+  public enderecoValido: boolean = true;
+  public numeroValido: boolean = true;
+  public complementoValido: boolean = true;
+  public formaPagamentoValido: boolean = true;
+
+  //estados primitivos do campos (pristine)
+  public enderecoEstadoPrimitivo: boolean = true;
+  public numeroEstadoPrimitivo: boolean = true;
+  public complementoEstadoPrimitivo: boolean = true;
+  public formaPagamentoEstadoPrimitivo: boolean = true;
+
+  //controlar botão confirmar compra
+  public formEstado: string = 'disable';
+
   constructor() { }
 
   ngOnInit() {
@@ -19,22 +34,42 @@ export class OrdemCompraComponent implements OnInit {
 
   public atulizaEndereco(endereco: string): void {
     this.endereco = endereco;
-    console.log(this.endereco);
+    this.enderecoEstadoPrimitivo = false;
+    this.enderecoValido = (this.endereco.length > 3) ? true : false;
+    this.habilitaForm();
   }
 
   public atulizaNumero(numero: string): void {
     this.numero = numero;
-    console.log(this.numero);
+    this.numeroEstadoPrimitivo = false;
+    this.numeroValido = (this.numero.length > 0) ? true : false;
+    this.habilitaForm();
   }
 
   public atulizaComplemento(complemento : string): void {
     this.complemento = complemento;
-    console.log(this.complemento);
+    this.complementoEstadoPrimitivo = false;
+    if(this.complemento.length > 0){
+      this.complementoValido = true;
+    }
+    this.habilitaForm();
   }
 
   public atualizaFormaPagamento(formaPagamento: string) : void {
     this.formaPagamento = formaPagamento;
-    console.log(this.formaPagamento);
+    this.formaPagamentoEstadoPrimitivo = false;
+    this.formaPagamentoValido = (this.formaPagamento != '') ? true : false;
+    this.habilitaForm();
+  }
+
+  public habilitaForm(): void{
+    if(this.enderecoValido
+    && this.numeroValido
+    && this.formaPagamento){
+      this.formEstado = '';
+    } else {
+      this.formEstado = 'disable'
+    }
   }
 
 }
