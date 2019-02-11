c6efa0f7a26714092f3a328909eefdcc727ae3b0
DanielDonato <daniel_donato10@yahoo.com.br>
2019-02-11 21:53:22 -0200

    Visual feedback in send form

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -11,6 +11,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public idPedidoCompra: number = undefined;
+
   public formulario: FormGroup = new FormGroup({
     'endereco': new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(120)]),
     'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
@@ -30,10 +32,14 @@ export class OrdemCompraComponent implements OnInit {
       this.formulario.get('complemento').markAsTouched();
       this.formulario.get('formaPagamento').markAsTouched();
     } else {
-      let pedido: Pedido = new Pedido(
+      const pedido: Pedido = new Pedido(
           this.formulario.value.endereco, this.formulario.value.numero,
           this.formulario.value.complemento, this.formulario.value.formaPagamento
          );
+      this.ordemCompraService.efetivarCompra(pedido)
+        .subscribe((idPedido: number) => {
+          this.idPedidoCompra = idPedido;
+        });
     }
   }
 }
