9d8e37fc9c579212ed6dd975285714b73a6b38d4
DanielDonato <daniel_donato10@yahoo.com.br>
2019-03-06 18:58:01 -0300

    Fixing instaciate of CarrinhoService

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,32 +1,38 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
+import { CarrinhoService } from '../carrinho.service';
+
 import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
-  providers: [OfertasService]
+  providers: [ OfertasService ]
 })
 export class OfertaComponent implements OnInit, OnDestroy {
 
-  public oferta:Oferta;
+  public oferta: Oferta;
 
-  constructor(private route:ActivatedRoute,
-     private ofertasService:OfertasService) { }
+  constructor(private route: ActivatedRoute,
+     private ofertasService: OfertasService,
+     private carrinhoService: CarrinhoService) { }
 
   ngOnInit() {
-
     this.route.params.subscribe((parametros: Params) => {
       this.ofertasService.getOfertaPorId(parametros.id)
-      .then( (oferta:Oferta) => {
+      .then( (oferta: Oferta) => {
         this.oferta = oferta;
       });
     });
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
+  }
+
+  public adicionarItemCarrinho(): void {
+    this.carrinhoService.incluirItem(this.oferta);
   }
 
 }
